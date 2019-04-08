8bf833873ff01f5a6abe9704723ec1419bddba2f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-08 11:51:23 +0200

    made sidebar and input field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
-import "./App.css"
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -11,23 +11,67 @@ class App extends Component {
           <ChatWindow />
         </Row>
       </div>
-    )
+    );
   }
 }
 
 const Sidebar = props => (
   <div className="sidebar fullHeight">
-    {[1, 2, 3, 4].map(item => (
-      <div className="contact-card">Hello {item}</div>
+    <div className="contact-card flex-row">
+      <Avatar file="./avatar.png" />
+      <div className="person-container">
+        <Name name="Person Name" />
+        <Description desc="LMAO ROFL...IPSUM" />
+      </div>
+    </div>
+    {[2, 3, 4, 5].map(item => (
+      <div className="contact-card" key={item}>
+        Hello {item}
+        {/* <Avatar />
+        <div className="person-container">
+          <Name name="Person Name" />
+          <Description desc="LMAO ROFL...IPSUM" />
+        </div> */}
+      </div>
     ))}
   </div>
-)
+);
 
-const ChatWindow = props => <div className="chatwindow fullHeight" />
+const ChatWindow = props => (
+  <div className="chatwindow fullHeight">
+    <InputField />
+  </div>
+);
 
 const Row = props => {
-  console.log(props)
-  return <div className="flex-row">{props.children}</div>
-}
+  console.log(props);
+  return <div className="flex-row">{props.children}</div>;
+};
 
-export default App
+const Avatar = props => {
+  if (props.file) {
+    return (
+      <div className="avatar">
+        <img src={props.file} alt="avatar" />
+      </div>
+    );
+  }
+  return <div className="avatar" />;
+};
+
+const Name = props => {
+  return <div className="personname">{props.name}</div>;
+};
+
+const Description = props => {
+  return <div className="description">{props.desc}</div>;
+};
+
+const InputField = props => {
+  return (
+    <div className="inputfield">
+      <input type="text" placeholder="You can write words here!" />
+    </div>
+  );
+};
+export default App;
