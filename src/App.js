70db98c98f7e3b5c71c0f3646c3d2bed4e915970
Oliver Weber <oliver@mondough.io>
2019-04-08 10:50:35 +0200

    starter code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Row>
+          <Sidebar />
+          <ChatWindow />
+        </Row>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const Sidebar = props => (
+  <div className="sidebar fullHeight">
+    {[1, 2, 3, 4].map(item => (
+      <div className="contact-card">Hello {item}</div>
+    ))}
+  </div>
+)
+
+const ChatWindow = props => <div className="chatwindow fullHeight" />
+
+const Row = props => {
+  console.log(props)
+  return <div className="flex-row">{props.children}</div>
+}
+
+export default App
