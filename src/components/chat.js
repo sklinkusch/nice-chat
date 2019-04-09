26516ad8337802096f7b2a6e98b58f1d2db85635
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-09 09:57:12 +0200

    outsourced chat.js

---
+++ src/components/chat.js
@@ -0,0 +1,16 @@
+import avatar from "../avatar.png";
+import avatar2 from "../avatar2.png";
+
+export default [
+  {
+    left: true,
+    file: { avatar },
+    message:
+      "Do you know that: Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus"
+  },
+  {
+    left: false,
+    file: { avatar2 },
+    message: "LMAO ROFL..IPSUM"
+  }
+];
