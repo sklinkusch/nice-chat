f0b8107c7daf14bc47d1bfa8a38521795eaa3503
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-09 09:49:35 +0200

    outsourced Sidebar

---
+++ src/components/ContactCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ContactCard = props => (
+  <div className="contact-card flex-row" key={props.item}>
+    <Avatar file={props.avatar} />
+    <div className="person-container">
+      <Name name={props.name} />
+      <Description desc={props.desc} />
+    </div>
+    <Dot color="bluey-purple" />
+  </div>
+);
+
+const Avatar = props => {
+  if (props.file) {
+    return (
+      <div className="avatar">
+        <img src={props.file} alt="avatar" />
+      </div>
+    );
+  }
+  return <div className="avatar" />;
+};
+
+const Name = props => {
+  return <div className="personname">{props.name}</div>;
+};
+
+const Description = props => {
+  return <div className="description">{props.desc}</div>;
+};
+
+const Dot = props => {
+  const klasse = `dot ${props.color}`;
+  return <div className={klasse} />;
+};
+
+export default ContactCard;
