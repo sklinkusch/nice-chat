85f904c071dd8474dc473491060f78be3e7b32a6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-09 10:28:54 +0200

    Updates

---
+++ precache-manifest.cbe0a1e26c067f3349d1f66323209295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "69f2b4639daef1948f009cc848dd8266",
+    "url": "/nice-chat/static/media/avatar2.69f2b463.png"
+  },
+  {
+    "revision": "d7cd886c7ec7e6c32f30798b7da36984",
+    "url": "/nice-chat/static/media/avatar.d7cd886c.png"
+  },
+  {
+    "revision": "c1ed0d7ccc49a67fe78e",
+    "url": "/nice-chat/static/js/runtime~main.28471973.js"
+  },
+  {
+    "revision": "a130bc29dd7ad6804533",
+    "url": "/nice-chat/static/js/main.c1956936.chunk.js"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/nice-chat/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "a130bc29dd7ad6804533",
+    "url": "/nice-chat/static/css/main.ef5225fb.chunk.css"
+  },
+  {
+    "revision": "56a4546287da1c6d4066619dccf61162",
+    "url": "/nice-chat/index.html"
+  }
+];
